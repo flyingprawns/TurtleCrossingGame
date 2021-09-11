d0e66146335030b99b346a6b063701f23be58bde
Qijie Zhu <qzhu9@ucsc.edu>
2021-09-11 14:11:48 -0700

    worked on turtle class abit

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.cars = []
+
+    def create_car(self):
+        pass
+
+
+class Car(Turtle):
+    def __init__(self):
+        super().__init__(shape="square")
+        self.color(random.choice(COLORS))
+        self.speed("fastest")
+        self.penup()
+        self.setheading(180)
+        self.turtlesize(stretch_len=2)
+        # TODO: starting position
